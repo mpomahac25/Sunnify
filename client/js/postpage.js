44d508411697d16bb7a7aebb5654ec95bbd10cde
Vadym <vglovny25@students.oamk.fi>
2026-03-25 00:47:11 +0200

    Add postpage.js script to post.html for dynamic post loading

---
+++ client/js/postpage.js
@@ -0,0 +1,10 @@
+const BACKEND_ROOT_URL = "http://127.0.0.1:3000"
+
+document.addEventListener("DOMContentLoaded", async () => {
+    const params = new URLSearchParams(window.location.search);
+    const postId = parseInt(params.get("id"));
+
+    try{
+
+    }catch(error){}
+})
