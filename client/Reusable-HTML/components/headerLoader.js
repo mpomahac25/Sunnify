0af3d29e4b2b59ba2f0f5592ad9e19e43547f823
mpomahac25 <mpomahac25@students.oamk.fi>
2026-03-29 11:59:28 +0300

    Moved login button logic
    Login button logic moved from main.js to headerLoader.js.

--- client/Reusable-HTML/components/headerLoader.js
+++ client/Reusable-HTML/components/headerLoader.js
@@ -1,24 +1,91 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const container = document.getElementById("header-container");
-    const brandHref = container?.dataset?.brandHref;
-    const loginText = container?.dataset?.loginText;
-    const loginHref = container?.dataset?.loginHref;
+const BACKEND_ROOT_URL = "http://127.0.0.1:3000";
 
-    fetch("/Reusable-HTML/components/header.html")
-        .then(response => response.text())
-        .then(html => {
-            if (!container) return;
-            container.innerHTML = html;
+//document.addEventListener("DOMContentLoaded", () => {
+//    const container = document.getElementById("header-container");
+//    const brandHref = container?.dataset?.brandHref;
+//    const loginText = container?.dataset?.loginText;
+//    const loginHref = container?.dataset?.loginHref;
 
-            if (brandHref) {
-                const brand = container.querySelector('.navbar-brand');
-                if (brand) brand.setAttribute('href', brandHref);
-            }
+//    fetch("/Reusable-HTML/components/header.html")
+//        .then(response => response.text())
+//        .then(html => {
+//            if (!container) return;
+//            container.innerHTML = html;
 
-            const loginBtn = container.querySelector('#login-logout-btn');
-            if (loginBtn) {
-                if (loginText) loginBtn.textContent = loginText;
-                if (loginHref) loginBtn.setAttribute('href', loginHref);
-            }
+//            if (brandHref) {
+//                const brand = container.querySelector('.navbar-brand');
+//                if (brand) brand.setAttribute('href', brandHref);
+//            }
+
+//            const loginBtn = container.querySelector('#login-logout-btn');
+//            if (loginBtn) {
+//                if (loginText) loginBtn.textContent = loginText;
+//                if (loginHref) loginBtn.setAttribute('href', loginHref);
+//            }
+//        });
+//});
+
+document.addEventListener("DOMContentLoaded", async () => {
+    try {
+        // Load header
+        const container = document.getElementById("header-container");
+
+        const res = await fetch("/Reusable-HTML/components/header.html");
+        const html = await res.text();
+        container.innerHTML = html;
+
+        // Set absolute href path(s)
+        const navbarBrand = container.querySelector(".navbar-brand");
+        navbarBrand.setAttribute("href", `${BACKEND_ROOT_URL}`)
+
+        const response = await fetch(`${BACKEND_ROOT_URL}/check-session`, {
+            method: "get",
+            credentials: "include"
         });
+
+        const result = await response.json();
+
+        if (result.loggedIn) {
+            // Change login button to logout button
+            const loginLogoutButton = container.querySelector("#login-logout-btn");
+
+            loginLogoutButton.textContent = "Logout";
+
+            loginLogoutButton.classList.remove("btn-outline-primary");
+            loginLogoutButton.classList.add("btn-outline-danger");
+
+            loginLogoutButton.removeAttribute("href");
+
+            loginLogoutButton.addEventListener("click", async (event) => {
+                event.preventDefault();
+
+                try {
+                    const response = await fetch(`${BACKEND_ROOT_URL}/logout`, {
+                        method: "post",
+                        credentials: "include"
+                    });
+
+                    const result = await response.json();
+
+                    if (!response.ok) {
+                        alert(result.error || "Logout failed");
+                        return;
+                    }
+
+                    //alert("Logout successful!");
+
+                    window.location.href = "main.html";
+                } catch (error) {
+                    alert("Unknown error occured!");
+                    console.log(`Network error: ${error}`);
+                }
+            })
+        }
+        else {
+            document.body.classList.remove("hidden");
+        }
+    } catch (error) {
+        console.error(error);
+        document.body.classList.remove("hidden");
+    }
 });
